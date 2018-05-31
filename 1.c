316d674b9e3fac30c8e85ebefa14ebc1d97ee7d5
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2018-05-31 14:53:59 +0430

    TRAFFIC LIGHT in codeVision

---
+++ 1.c
@@ -0,0 +1,234 @@
+/*****************************************************
+This program was produced by the
+CodeWizardAVR V2.05.3 Standard
+Automatic Program Generator
+© Copyright 1998-2011 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 18/01/2017
+Author  : Ehsan
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 1/000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*****************************************************/
+#include <stdlib.h>
+#include <mega16.h>
+#include <delay.h>
+// Alphanumeric LCD functions
+#include <alcd.h>
+int counter=0,sg=0,sr=0,grnBg=0,redBg=0;
+char redMain[],grnMain[];
+// Timer1 overflow interrupt service routine
+interrupt [TIM1_OVF] void timer1_ovf_isr(void)
+{
+ 
+  if(counter<20)
+        counter++;
+    else
+    {   
+        counter=0;
+        if(PORTC.0==1){
+            sr++;
+        }
+        if(PORTC.2==1){
+            sg++;
+        }
+        if(PORTC.0==1 && PINB.0==1){
+            redBg--;
+        } 
+        if(PORTC.2==1 && PINB.2==1){
+            grnBg--;
+        }  
+    }
+    TCNT1=0x3CAF;
+// Place your code here
+
+}
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTA=0x00;
+DDRA=0x00;
+
+// Port B initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTB=0x00;
+DDRB=0x00;
+
+// Port C initialization
+// Func7=Out Func6=Out Func5=Out Func4=Out Func3=Out Func2=Out Func1=Out Func0=Out 
+// State7=0 State6=0 State5=0 State4=0 State3=0 State2=0 State1=0 State0=0 
+PORTC=0x00;
+DDRC=0xFF;
+
+// Port D initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTD=0x00;
+DDRD=0x00;
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=0x00;
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: 1000/000 kHz
+// Mode: Normal top=0xFFFF
+// OC1A output: Discon.
+// OC1B output: Discon.
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: On
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=0x00;
+TCCR1B=0x01;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0x00;
+TCCR2=0x00;
+TCNT2=0x00;
+OCR2=0x00;
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=0x00;
+MCUCSR=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=0x04;
+
+// USART initialization
+// USART disabled
+UCSRB=0x00;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// Analog Comparator Input Capture by Timer/Counter 1: Off
+ACSR=0x80;
+SFIOR=0x00;
+
+// ADC initialization
+// ADC disabled
+ADCSRA=0x00;
+
+// SPI initialization
+// SPI disabled
+SPCR=0x00;
+
+// TWI initialization
+// TWI disabled
+TWCR=0x00;
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTA Bit 0
+// RD - PORTA Bit 1
+// EN - PORTA Bit 2
+// D4 - PORTA Bit 4
+// D5 - PORTA Bit 5
+// D6 - PORTA Bit 6
+// D7 - PORTA Bit 7
+// Characters/line: 8
+lcd_init(8);
+
+// Global enable interrupts
+#asm("sei")
+
+while (1)
+      {
+            if(PINB.0==1 && PORTC.0==0){                 
+                    sr=0;
+                    PORTC.0=1; 
+            }
+            redBg=sr;                
+            if(PINB.2==1 && PORTC.0==1 ){            
+                    sg=0;
+                    PORTC.0=0;
+                    PORTC.2=1;               
+            }
+            grnBg=sg;                          
+            if(PINB.0==1 && PORTC.2==1){ 
+                    PORTC.1=1;
+                    if(PORTC.1==1){
+                        L2:
+                        PORTC.2=0;
+                        lcd_puts("Caution!");
+                        delay_ms(3000);
+                        lcd_clear();
+                        PORTC.1=0;
+                        sr=0;
+                        PORTC.0=1;
+                    }                    
+                    while(redBg>=0){
+                        itoa(redBg,redMain); 
+                        lcd_puts(redMain);
+                        lcd_clear();
+                        if(PINB.2==1){
+                            redBg=sr;
+                            goto L1;
+                        }
+                    }                 
+            redBg=sr;        
+            } 
+            
+            L1:
+            if(PINB.2==1 && PORTC.0==1){
+                    PORTC.0=0;
+                    sg=0;
+                    PORTC.2=1;
+                    while(grnBg>=0){
+                        itoa(grnBg,grnMain);
+                        lcd_puts(grnMain);
+                        lcd_clear();
+                        if(PINB.0==1){
+                            grnBg=sg;
+                            goto L2;  
+                        }
+                        
+                    }
+            grnBg=sg;
+            }
+             
+      }
+}
